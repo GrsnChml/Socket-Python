9340ff34392ba582c7a289903c90b7b0dae83716
Grsn_Chml <sysgrsn@gmail.com>
2019-10-18 14:52:34 -0600

    cambios

--- socket_re.py
+++ socket_re.py
@@ -10,13 +10,14 @@ conn = MySQLdb.connect(host= "spaceapp.ch7oeqwewnpa.us-west-2.rds.amazonaws.com"
                   db="robot_db")
 
    
-current_time = datetime.datetime.now(pytz.timezone('America/Guatemala')) 
+
 
 UDP_IP = "192.168.0.105"
 UDP_PORT = 50012
   
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 sock.bind((UDP_IP, UDP_PORT)) 
+x = conn.cursor()
 
 def stripped(s):
     return "".join(i for i in s if 31 < ord(i) < 127)
@@ -24,20 +25,22 @@ def stripped(s):
 
 def sendDataToDataBase(longitud,latitud,velocidad):
     try:
-        x = conn.cursor()
+        current_time = datetime.datetime.now(pytz.timezone('America/Guatemala')) 
+        
         x.execute("""INSERT INTO trama VALUES (null,%s,%s,%s,%s)""",(latitud,longitud,velocidad,current_time));
         print("success");
         conn.commit();
-        conn.close();
+        
     except Exception as error:
         conn.rollback();
         print("error " + error);
-        conn.close();
+        
 
   
 while True: 
     data, addr = sock.recvfrom(1024);
     data = stripped(data.decode('utf-8')).strip()
     data1 = data.split(',')
-    sendDataToDataBase(data1[11],data1[12],data1[19]);
+    if data1[11] and data1[12] and data1[19]:
+        sendDataToDataBase(data1[11],data1[12],data1[19]);
 
