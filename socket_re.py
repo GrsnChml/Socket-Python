480987bec7e1ca0d637620b61e1ef0673184e004
Grsn_Chml <sysgrsn@gmail.com>
2019-10-18 11:24:43 -0600

    change code

--- socket_re.py
+++ socket_re.py
@@ -4,7 +4,6 @@ import MySQLdb
 import datetime 
 import pytz 
 
-  
 conn = MySQLdb.connect(host= "spaceapp.ch7oeqwewnpa.us-west-2.rds.amazonaws.com",
                   user="admin",
                   passwd="1234abcd",
@@ -13,23 +12,19 @@ conn = MySQLdb.connect(host= "spaceapp.ch7oeqwewnpa.us-west-2.rds.amazonaws.com"
    
 current_time = datetime.datetime.now(pytz.timezone('America/Guatemala')) 
 
-
-UDP_IP = "192.241.219.11"
+UDP_IP = "192.168.0.105"
 UDP_PORT = 50012
   
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 sock.bind((UDP_IP, UDP_PORT)) 
 
-
 def stripped(s):
     return "".join(i for i in s if 31 < ord(i) < 127)
+
 
 def sendDataToDataBase(longitud,latitud,velocidad):
     try:
         x = conn.cursor()
-        print(longitud)
-        print(latitud)
-        print(velocidad)
         x.execute("""INSERT INTO trama VALUES (null,%s,%s,%s,%s)""",(latitud,longitud,velocidad,current_time));
         print("success");
         conn.commit();
@@ -45,5 +40,4 @@ while True:
     data = stripped(data.decode('utf-8')).strip()
     data1 = data.split(',')
     sendDataToDataBase(data1[11],data1[12],data1[19]);
-    break;
 
